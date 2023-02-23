fa713f561ae4bde80ef97999587ea9605985bea1
Erkut Cakmak <cakmak.erkut1@gmail.com>
2023-02-22 22:16:11 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+saturdayFun();
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+mondayWork();
+
+function wrapAdjective(flair = "*") {
+    return function (noun = "special") {
+        return `You are ${flair}${noun}${flair}!`
+    }
+}
+
+wrapAdjective('*')("a dedicated programmer");
+wrapAdjective('||')("a dedicated programmer");
